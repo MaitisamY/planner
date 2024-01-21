4dfa718c3fc8b37a28d230cc81a4484da0df2d50
Muhammad Aitisam Yaseen <m.aitisamyaseen@gmail.com>
2024-01-22 02:48:02 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 
 export const metadata = {
   title: 'PLANNER',
-  description: 'PLANNER - A simple daily tasks maintainer app with task and its date creation, edition, mark or unmark as completed or pending and deletion features.',
+  description: 'A simple daily tasks maintainer app with task and its date creation, edition, mark or unmark as completed or pending and deletion features.',
   author: 'Aitisam Yaseen',
 }
 
