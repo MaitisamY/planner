f05ccdaeff334ba600c7c2810e5a2edd2f700ec2
Muhammad Aitisam Yaseen <m.aitisamyaseen@gmail.com>
2024-01-22 02:41:45 +0500

    Title update

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>{metadata.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
