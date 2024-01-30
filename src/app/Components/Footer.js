4cc1434533ec3b50f4bbb5bbdd885e351e6f0efc
Muhammad Aitisam Yaseen <m.aitisamyaseen@gmail.com>
2024-01-31 00:39:36 +0500

    remove name from footer for now

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -1,13 +1,13 @@
 export default function Footer() {
-    const gotoMyPortfolio = () => {
-        window.open("https://github.com/MaitisamY/", "_blank");
-    }
+    // const gotoMyPortfolio = () => {
+    //     window.open("https://github.com/MaitisamY/", "_blank");
+    // }
     return (
         <footer>
             <p>&copy; {new Date().getFullYear()}, PLANNER.</p>
-            <span>Made with ❤️ by 
+            {/* <span>Made with ❤️ by 
                 <a target="_blank" rel="noopener noreferrer" onClick={gotoMyPortfolio}> Aitisam Yaseen</a>
-            </span>
+            </span> */}
         </footer>
     );
 }
